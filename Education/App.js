9f925cc110ab97c2f9d7fb7634c623609e949e76
Viraj Patel <vpatel@Virajs-Air.cable.rcn.com>
2024-06-13 13:13:46 -0400

    Initial commit

--- Education/App.js
+++ Education/App.js
@@ -1,14 +1,22 @@
 // React function component to render a list of class names
 
 function ClassList() {
-  const classes = ['Biology', 'Chemistry', 'Physics', 'Mathematics'];
+  const classes = [
+    { "img": "basketball.png", "description": "I like to play"},
+    { "img": "traveling.png", "description": "I like to play"},
+    { "img": "gym.png", "description": "I like to play"},
+    { "img": "photography.png", "description": "I like to play"}
+  ]
+  
   
   return (
     <div className="container">
       
           {classes.map((className, index) => (
-       
-              <div className="card" key={index}>{className}</div>
+              <div className="card" key={index}>
+                <img src={className.img}></img>
+                <div>{className.description}</div>
+              </div>
           ))}
       
       </div>
@@ -17,3 +25,25 @@ function ClassList() {
 
 // Render the ClassList component in the 'app' div
 ReactDOM.render(<ClassList />, document.getElementById('app'));
+
+
+/*
+import inventoryData from "./slideshow.json";
+import React, { Component } from "react";
+
+class Slider extends Component{
+    render() {
+        return (
+            inventoryData.map((item) => (
+                <p class="Slider">
+                    <li>
+                        <p>SKU: {item.description}</p>
+                    </li>
+                </p>
+            )) 
+        );
+    }
+}
+
+export default Slider;
+*/
