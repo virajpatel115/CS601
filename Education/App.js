29dbbf7c12cc2549f4d6c9ba0148ce8c499fcf41
Viraj Patel <vpatel@Virajs-Air.cable.rcn.com>
2024-06-13 13:32:40 -0400

    Initial commit

--- Education/App.js
+++ Education/App.js
@@ -1,27 +1,34 @@
 // React function component to render a list of class names
 
-function ClassList() {
-  const classes = [
-    { "img": "basketball", "description": "I like to play"},
-    { "img": "traveling", "description": "I like to play"},
-    { "img": "gym", "description": "I like to play"},
-    { "img": "photography", "description": "I like to play"}
-  ]
+import React, { Component } from "react";
+
+class ClassList extends Component {
+  render() {
+    const classes = [
+      { "img": "basketball", "description": "I like to play"},
+      { "img": "traveling", "description": "I like to play"},
+      { "img": "gym", "description": "I like to play"},
+      { "img": "photography", "description": "I like to play"}
+    ];
+    
   
+    return (
+      <div className="container">
+            {classes.map((className, index) => (
+                <div className="card" key={index}>
+                  <img key={image} src={require('./${image}.png')} clasName="my-img"></img>
+                  <div>{className.description}</div>
+                </div>
+            ))}
+        
+        </div>
+    );}
+  }
   
-  return (
-    <div className="container">
-      
-          {classes.map((className, index) => (
-              <div className="card" key={index}>
-                <img src={require('./' + className.img + '.png')}></img>
-                <div>{className.description}</div>
-              </div>
-          ))}
-      
-      </div>
-  );
-}
+  export default ClassList;
+
+
+  
 
 // Render the ClassList component in the 'app' div
 ReactDOM.render(<ClassList />, document.getElementById('app'));
