b852f105623ea2dd55b3966a4dadc64a0ad16720
Viraj Patel <vpatel@Virajs-Air.cable.rcn.com>
2024-06-14 16:10:01 -0400

    Initial commit

--- Education/App.js
+++ Education/App.js
@@ -1,20 +1,23 @@
 // React function component to render a list of class names
 
 function ClassList() {
-  const under = ['Biology', 'Chemistry', 'Physics', 'Mathematics'];
-  const masters = ['Biology', 'Chemistry', 'Physics', 'Mathematics'];
+  const under = ['Data Science', 'Database Design & Practices', 'Information Systems in Management', 'Computer Networks', "Scripting Languages"];
+  const masters = ['Data Structure & Algorithms', 'Advanced Programming Techniques', 'Software Design & Patterns', 'Information Structures with Python'];
 
   return (
     <div className="app-container">
-      <h2>My Education</h2>
-      <h3>Undergraduate</h3>
+      <h2>Undergraduate</h2>
+      <h3>BS in Information Systems, Stony Brook University</h3>
+      <p>Graduation Date: 2019</p>
       <div className="under">
         {under.map((className, index) => (
             <div className="card" key={index}>{className}</div>
         ))}   
       </div>
 
-      <h3>Graduate</h3>
+      <h2>Graduate</h2>
+      <h3>MS in Software Development, Boston University</h3>
+      <p>Graduation Date: December 2024 (Expected)</p>
       <div className="masters">
         {under.map((className, index) => (
           <div className="card" key={index}>{className}</div>
