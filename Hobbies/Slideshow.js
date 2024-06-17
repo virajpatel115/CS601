84fbca32e81bbfa441afe8679691ac78b2267f2c
Viraj Patel <vpatel@Virajs-Air.cable.rcn.com>
2024-06-17 13:11:47 -0400

    Initial commit

--- Hobbies/Slideshow.js
+++ Hobbies/Slideshow.js
@@ -13,15 +13,11 @@ function numSlide(x) {
 function show(x) {
     let i;
     let slides = document.getElementsByClassName("my-slides");
-    let dots = document.getElementsByClassName("dots");
     if (x > slides.length) { index =1 }
     if (x < 1) { index = slides.length }
     for (i=0; i< slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i=0; i< dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
+
     slides[index-1].style.display = "block";
-    dots[index-1].className += " active";
 }
